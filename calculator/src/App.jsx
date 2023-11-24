860b49ff847be9f691f7cfdeaa142464378475ea
Atlas <antonio.claudino@fido.com.vc>
2023-11-23 21:53:13 -0300

    done project

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [input, setInput] = useState("");
+  const buttons = [
+    "AC",
+    "DEL",
+    "%",
+    "/",
+    "7",
+    "8",
+    "9",
+    "*",
+    "4",
+    "5",
+    "6",
+    "-",
+    "1",
+    "2",
+    "3",
+    "+",
+    "00",
+    "0",
+    ".",
+    "=",
+  ];
+
+  const handleClick = (value) => {
+    if (value === "=") {
+      setInput(eval(input).toString());
+    } else if (value === "AC") {
+      setInput("");
+    } else if (value === "DEL") {
+      setInput(input.slice(0, -1));
+    } else {
+      setInput(input + value);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="calculator">
+        <input
+          type="text"
+          id="inputBox"
+          value={input}
+          placeholder="0"
+          readOnly
+        />
+        <div>
+          {buttons.map((button, index) => (
+            <button
+              key={index}
+              className={`button ${isNaN(button) ? "operator" : ""}`}
+              onClick={() => handleClick(button)}
+            >
+              {button}
+            </button>
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    </div>
+  );
+};
